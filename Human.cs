e54e0b1ef7e04ea0b6799836b82688be8dad9e4d
gideon <edoghotugiddy@gmail.com>
2022-07-18 11:27:07 +0100

    added virtual to all the props

--- Human.cs
+++ Human.cs
@@ -8,10 +8,10 @@ namespace HelloGitWorld
 {
     public class Human
     {
-        public int Id { get; set; }
-        public DateTime Created { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
+        public virtual int Id { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual string? Name { get; set; }
+        public virtual string? Address { get; set; }
         public string? PhoneNumber { get; set; }
         
 
